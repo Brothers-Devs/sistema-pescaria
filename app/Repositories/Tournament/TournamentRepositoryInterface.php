19d046c9fe1b9be240ead740e4c47c3058808029
cabralti <cabral.9santos@gmail.com>
2023-04-30 15:20:30 -0300

    feat(SDP-18): cria repository para busca um torneio (tournament)

--- app/Repositories/Tournament/TournamentRepositoryInterface.php
+++ app/Repositories/Tournament/TournamentRepositoryInterface.php
@@ -3,8 +3,11 @@
 namespace App\Repositories\Tournament;
 
 use App\DTO\CreateTournamentDTO;
+use stdClass;
 
 interface TournamentRepositoryInterface
 {
+    public function getById(string $id): ?stdClass;
+
     public function create(CreateTournamentDTO $createTournamentDTO);
 }
