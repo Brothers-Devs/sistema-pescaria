d4f67cb7b905c790be5ca5ecdda1c6f64bc30c44
cabralti <cabral.9santos@gmail.com>
2023-04-30 01:03:47 -0300

    feat(SDP-17): realiza o bind da interface de repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\Tournament\Eloquent\TournamentRepository;
+use App\Repositories\Tournament\TournamentRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(TournamentRepositoryInterface::class, TournamentRepository::class);
     }
 
     /**
