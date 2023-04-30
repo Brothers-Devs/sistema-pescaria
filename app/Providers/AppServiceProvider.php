13cfc9afd389a123bc1f42922455d1c7e81d448d
cabralti <cabral.9santos@gmail.com>
2023-04-30 19:26:22 -0300

    feat(SDP-23): cria repository interface category
    - cria classe concreta do eloquent para acessar a model category
    - realiza o bind

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\Category\CategoryRepositoryInterface;
+use App\Repositories\Category\Eloquent\CategoryRepository;
 use App\Repositories\Tournament\Eloquent\TournamentRepository;
 use App\Repositories\Tournament\TournamentRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
@@ -14,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(TournamentRepositoryInterface::class, TournamentRepository::class);
+        $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
     }
 
     /**
