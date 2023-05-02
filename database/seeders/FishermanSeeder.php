cbc6c069814545beea1af40ce9802fb7ed18d599
cabralti <cabral.9santos@gmail.com>
2023-05-01 22:41:09 -0300

    feat(SDP-24): cria model Fisherman e ajusta migration
    - cria tambem seeder e factory para popular banco

---
+++ database/seeders/FishermanSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Fisherman;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FishermanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Fisherman::factory(2)->create();
+    }
+}
