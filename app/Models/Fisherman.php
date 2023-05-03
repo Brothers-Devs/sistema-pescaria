8417be3cf81d50885c79055022738b1c43e2ed6c
cabralti <cabral.9santos@gmail.com>
2023-05-03 01:16:16 -0300

    feat(SDP-25): cria mutators para cpf e phone

--- app/Models/Fisherman.php
+++ app/Models/Fisherman.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
+use App\Helper\Helper;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Fisherman extends Model
 {
     use HasFactory;
+
+    /**
+     * @return Attribute
+     */
+    protected function cpf(): Attribute
+    {
+        return Attribute::make(
+            get: fn(?string $value) => $value ? Helper::addMaskForCpf($value) : null
+        );
+    }
+
+    /**
+     * @return Attribute
+     */
+    protected function phone(): Attribute
+    {
+        return Attribute::make(
+            get: fn(?string $value) => $value ? Helper::addMaskForPhoneBr($value) : null
+        );
+    }
 }
