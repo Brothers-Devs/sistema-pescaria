c9d6dfacbe8d2256a385f254b33edadd8ad8de2d
cabralti <cabral.9santos@gmail.com>
2023-05-06 01:55:39 -0300

    feat(SDP-38): cria dto de atualizacao de equipe

---
+++ app/DTO/Team/UpdateTeamDTO.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DTO\Team;
+
+use App\Http\Requests\StoreUpdateTeamRequest;
+
+class UpdateTeamDTO
+{
+    public function __construct(
+        public int    $id,
+        public string $name,
+        public string $type,
+        public int    $tournamentId,
+        public int    $categoryId
+    )
+    {
+    }
+
+    /**
+     * @param StoreUpdateTeamRequest $request
+     * @param int $id
+     * @return UpdateTeamDTO
+     */
+    public static function makeFromRequest(StoreUpdateTeamRequest $request, int $id): UpdateTeamDTO
+    {
+        return new self(
+            $id,
+            $request->name,
+            $request->type,
+            $request->tournament_id,
+            $request->category_id,
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->name,
+            'type' => $this->type,
+            'tournament_id' => $this->tournamentId,
+            'category_id' => $this->categoryId
+        ];
+    }
+}
