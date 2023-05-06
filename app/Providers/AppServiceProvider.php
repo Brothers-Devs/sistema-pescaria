14da5a3b432984a1a2ef4a1f662c653e585e6f89
cabralti <cabral.9santos@gmail.com>
2023-05-05 22:46:02 -0300

    feat(SDP-36): cria repository interface para Team
    - realiza o bind no AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use App\Repositories\Category\CategoryRepositoryInterface;
 use App\Repositories\Category\Eloquent\CategoryRepository;
 use App\Repositories\Fisherman\Eloquent\FishermanRepository;
 use App\Repositories\Fisherman\FishermanRepositoryInterface;
+use App\Repositories\Team\Eloquent\TeamRepository;
+use App\Repositories\Team\TeamRepositoryInterface;
 use App\Repositories\Tournament\Eloquent\TournamentRepository;
 use App\Repositories\Tournament\TournamentRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
@@ -20,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(TournamentRepositoryInterface::class, TournamentRepository::class);
         $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
         $this->app->bind(FishermanRepositoryInterface::class, FishermanRepository::class);
+        $this->app->bind(TeamRepositoryInterface::class, TeamRepository::class);
     }
 
     /**
