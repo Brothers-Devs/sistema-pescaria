135a4bf4fd1d6279f48fd5d16d2a4dc97e843a74
cabralti <cabral.9santos@gmail.com>
2023-05-12 15:32:34 -0300

    fix: Retira props não utilizada

--- frontend/src/app/views/screenskit/teams/EditTeam.jsx
+++ frontend/src/app/views/screenskit/teams/EditTeam.jsx
@@ -65,7 +65,6 @@ export default function EditTeam() {
                         typeEditOrCreateForm={typeEditForm}
                         valuesInputs={valuesInputs}
                         setValuesInputs={setValuesInputs}
-                        detailsTeam={valuesInputs}
                     />
                 </ContainerRoot>
             ) : null}
