bc60440f13b595fd1faa288f322d3dd35c26b544
cabralti <cabral.9santos@gmail.com>
2023-05-15 15:04:30 -0300

    feat:Implementa componente de listagem de equipes disponiveis para apuração

---
+++ frontend/src/app/views/screenskit/verification/formsTableCreate/CreationContent.jsx
@@ -0,0 +1,101 @@
+import {
+    Autocomplete,
+    Box,
+    Divider,
+    TextField,
+    Typography,
+} from "@mui/material";
+import styled from "@emotion/styled";
+import instance from "../../../../../axios"
+import { useEffect, useState } from "react";
+import FormsTable from "./FormsTable";
+
+
+export default function CreationContent({ typeEditOrCreateForm }) {
+    const [teamsAvailable, setTeamsAvailable] = useState()
+    const [modificationInputTeam, setModificationInputTeamset] = useState(false)
+    const [dataApurathion, setDataApurathion] = useState({
+        team_id: "",
+        name: "",
+        fisheries: []
+    })
+    const dataTeamsAvailableCompact = teamsAvailable?.map(teamAvailable => {
+        return { id: teamAvailable.id, name: `${teamAvailable.id} - ${teamAvailable.name}` }
+    })
+
+    function handleOnChange(value, key) {
+        setDataApurathion({ ...dataApurathion, team_id: value.id, [key]: value.name })
+        setModificationInputTeamset(!modificationInputTeam)
+    }
+
+    useEffect(() => {
+        const promiseTeamsAvailable = instance.get("/teams/available")
+        promiseTeamsAvailable.then((res) => {
+            setTeamsAvailable(res.data.data)
+        }).catch((error) => {
+            console.log(error)
+        })
+    }, [modificationInputTeam])
+
+
+    return (
+        <FormVerification >
+            <Box
+                sx={{
+                    paddingTop: 3,
+                    paddingLeft: 3,
+                    display: "flex",
+                    alignItems: "center",
+                    marginBottom: 3,
+                }}
+            >
+                <Typography variant="h6">Registrar Nova Apuração</Typography>
+            </Box>
+            <Divider variant="fullWidth" />
+            <Box sx={{
+                marginTop: 5,
+                paddingLeft: 3,
+                paddingBottom: 5,
+            }}>
+                <Typography variant="subtitle2" sx={{ mb: 2 }} >Obs: Aqui são listadas somente as equipes que não possuem apurações cadastradas.</Typography>
+                <Typography variant="subtitle1" sx={{ mb: 2 }}>Equipe</Typography>
+                <Autocomplete
+                    disableClearable
+                    noOptionsText="Sem Registro"
+                    id="combo-box-demo"
+                    options={dataTeamsAvailableCompact || [{ name: "" }]}
+                    value={dataApurathion}
+                    onChange={(_, newValue) => handleOnChange(newValue, "name")}
+                    getOptionLabel={(option) => option.name || dataApurathion.name}
+                    renderOption={(props, option) => (
+                        <li {...props} key={option.name}>
+                            {option.name}
+                        </li>
+                    )}
+                    sx={{ width: 400 }}
+                    isOptionEqualToValue={(option, value) =>
+                        value === undefined || value === "" || value.name === option.name
+                    }
+                    renderInput={(params) =>
+                        <TextField {...params} label="Equipe" />
+                    }
+                />
+            </Box>
+            {dataApurathion.name !== "" ? <FormsTable typeEditOrCreateForm={typeEditOrCreateForm} dataApurathion={dataApurathion} setDataApurathion={setDataApurathion} modificationInputTeam={modificationInputTeam} /> : null}
+        </FormVerification >
+    );
+}
+
+
+
+
+const FormVerification = styled("form")(() => ({
+    marginTop: "10px",
+
+    background: "#FFFFFF",
+    width: "100%",
+    height: "100%",
+    boxSizing: "border-box",
+    boxShadow: "0 3px 10px rgb(0 0 0 / 0.2)",
+    borderRadius: "5px",
+}));
