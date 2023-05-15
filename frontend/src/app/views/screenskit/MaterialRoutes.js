67bb666411faa7ca40401d2e4985085d9238f38a
cabralti <cabral.9santos@gmail.com>
2023-05-15 14:59:33 -0300

    feat: Implementa o roteamento para tela de listagem e cadastro de apurações

--- frontend/src/app/views/screenskit/MaterialRoutes.js
+++ frontend/src/app/views/screenskit/MaterialRoutes.js
@@ -6,6 +6,8 @@ const AppTournaments = Loadable(lazy(() => import('./tournaments/AppTournaments'
 const AppTeams = Loadable(lazy(() => import('./teams/AppTeams')));
 const CreateTeams = Loadable(lazy(() => import('./teams/CreateTeam')));
 const EditTeam = Loadable(lazy(() => import('./teams/EditTeam')));
+const AppVerification = Loadable(lazy(() => import('./verification/AppVerification')));
+const CreateVerification = Loadable(lazy(() => import('./verification/formsTableCreate/CreateVerification')));
 const AppReports = Loadable(lazy(() => import('./reports/AppReports')));
 const CreateFisherman = Loadable(lazy(() => import('./fishermen/CreateFisherman')));
 const EditFisherman = Loadable(lazy(() => import('./fishermen/EditFisherman')));
@@ -30,6 +32,14 @@ const materialRoutes = [
   {
     path: '/dashboard/equipes/editar/:id',
     element: <EditTeam />,
+  },
+  {
+    path: '/dashboard/apuracoes',
+    element: <AppVerification />,
+  },
+  {
+    path: '/dashboard/apuracoes/cadastrar',
+    element: <CreateVerification />,
   },
   {
     path: '/dashboard/relatorios',
