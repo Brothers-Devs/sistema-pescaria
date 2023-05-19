f2d7ffd26ddaf94c804d488e1b62db182189f87f
cabralti <cabral.9santos@gmail.com>
2023-05-19 20:23:54 -0300

    feat:Adiciona butão para baixar relatório de pescadores

--- frontend/src/app/utils/TableUtils.jsx
+++ frontend/src/app/utils/TableUtils.jsx
@@ -9,6 +9,7 @@ import { AiOutlineInbox } from "react-icons/ai";
 import ButtonUtils from "./ButtonUtils";
 import { Button, CircularProgress, Tooltip, Typography } from "@mui/material";
 import { FcPrint } from "react-icons/fc";
+import { Link } from "react-router-dom";
 function QuickSearchToolbar() {
     return (
         <Box
@@ -71,17 +72,21 @@ function CustomNoRows() {
 }
 
 export default function TableUtils({ dataContent, columns, setRowSelected, path, type, titleToolTip }) {
+    const LINK_OF_REPORTS_FISHERMEN = "http://localhost:8000/api/reports/fishermen"
+
     return (
         <Box sx={{ height: 760, width: 1 }}>
             {type === "relatorio-pescadores" ?
                 <Box sx={{ display: "flex", width: 1, justifyContent: "flex-end", alignItems: "center" }}>
-                    <Box sx={{ display: "flex", minWidth: 200, pl: 105 }}>
-                        <Tooltip title="Baixar Relatório de Pescadores">
-                            <Button size="large" variant="contained" sx={{ width: 350, height: 42, mr: 0 }}>
-                                <FcPrint size={25} />
-                                <Typography variant="subtitle1" sx={{ ml: 1, width: 1 }}>Download (.pdf)</Typography>
+                    <Tooltip title="Baixar Relatório de Pescadores">
+                        <Link to={LINK_OF_REPORTS_FISHERMEN} target="_blank">
+                            <Button size="large" variant="contained" sx={{ mb: 2, mr: 2 }}>
+                                <FcPrint size={25} style={{ marginRight: 10 }} />
+                                Download (.pdf)
                             </Button>
-                        </Tooltip>
+                        </Link>
+                    </Tooltip>
+                    <Box sx={{ display: "flex" }}>
                         <ButtonUtils
                             functionButton="Cadastrar"
                             path={path}
