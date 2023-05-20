2fdab0efe8a4f6aca697564e30a312febb43621a
cabralti <cabral.9santos@gmail.com>
2023-05-19 21:17:30 -0300

    fix:Adiciona variavel no array de dependencia do useMemo

--- frontend/src/app/views/screenskit/fishermen/AppFisherman.jsx
+++ frontend/src/app/views/screenskit/fishermen/AppFisherman.jsx
@@ -73,7 +73,7 @@ export default function AppFisherman() {
                 disableClickEventBubbling: true,
             },
         ],
-        [rowSelected]
+        [rowSelected, modification]
     );
 
     useEffect(() => {
