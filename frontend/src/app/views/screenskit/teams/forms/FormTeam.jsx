577ef954028d96f9edbc167dcf4dfe7f24bcc405
lucasscabral <lucas.9cabral@gmail.com>
2024-04-29 20:30:47 -0300

    Refactor: Retira o campo de categoria, o objeto de categorias e a lógica de escolha de uma categoria

--- frontend/src/app/views/screenskit/teams/forms/FormTeam.jsx
+++ frontend/src/app/views/screenskit/teams/forms/FormTeam.jsx
@@ -10,39 +10,19 @@ import {
 } from "@mui/material";
 import styled from "@emotion/styled";
 
-const categorys = [
-    {
-        "id": 1,
-        "name": "Categoria Especial - Modalidade Arremesso",
-    },
-    {
-        "id": 2,
-        "name": "Categoria Comum - Modalidade Arremesso e Corrico",
+const types = ["DUPLA", "TRIO"];
 
-    }
-];
-const types = [
-    "DUPLA",
-    "TRIO",
-];
-
-
-export default function FormTeam({ typeEditOrCreateForm, valuesInputs, setValuesInputs }) {
-
+export default function FormTeam({
+    typeEditOrCreateForm,
+    valuesInputs,
+    setValuesInputs,
+}) {
     function handleOnChange(value, key) {
-        if (key === "category") {
-            if (value === "Categoria Especial - Modalidade Arremesso") {
-                setValuesInputs({ ...valuesInputs, [key]: { id: 1, name: value } });
-            } else {
-                setValuesInputs({ ...valuesInputs, [key]: { id: 2, name: value } });
-            }
-        } else {
-            setValuesInputs({ ...valuesInputs, [key]: value });
-        }
+        setValuesInputs({ ...valuesInputs, [key]: value });
     }
 
     return (
-        <FormDetailsTeam >
+        <FormDetailsTeam>
             <Box
                 sx={{
                     paddingTop: 3,
@@ -58,10 +38,12 @@ export default function FormTeam({ typeEditOrCreateForm, valuesInputs, setValues
                 </Typography>
             </Box>
             <Divider variant="fullWidth" />
-            <Box sx={{
-                paddingLeft: 3,
-                display: "flex",
-            }}>
+            <Box
+                sx={{
+                    paddingLeft: 3,
+                    display: "flex",
+                }}
+            >
                 <Box
                     sx={{
                         marginTop: 3,
@@ -69,10 +51,12 @@ export default function FormTeam({ typeEditOrCreateForm, valuesInputs, setValues
                         width: 560,
                         display: "flex",
                         justifyContent: "center",
-                        flexDirection: "column"
+                        flexDirection: "column",
                     }}
                 >
-                    <Typography variant="subtitle1" sx={{ mb: 2 }}>Nome</Typography>
+                    <Typography variant="subtitle1" sx={{ mb: 2 }}>
+                        Nome
+                    </Typography>
                     <TextField
                         sx={{ width: 250 }}
                         value={valuesInputs?.name}
@@ -90,65 +74,37 @@ export default function FormTeam({ typeEditOrCreateForm, valuesInputs, setValues
                         width: 500,
                         display: "flex",
                         justifyContent: "center",
-                        flexDirection: "column"
+                        flexDirection: "column",
                     }}
                 >
-                    <Typography variant="subtitle1" sx={{ mb: 2 }}>Tipo</Typography>
+                    <Typography variant="subtitle1" sx={{ mb: 2 }}>
+                        Tipo
+                    </Typography>
                     <FormControl required sx={{ width: 190 }}>
                         <InputLabel id="type">Tipo</InputLabel>
                         <Select
                             labelId="type"
                             id="select-type"
                             value={
-                                valuesInputs?.type !== "" ? valuesInputs.type : ""
+                                valuesInputs?.type !== ""
+                                    ? valuesInputs.type
+                                    : ""
                             }
                             label="Tipo"
                             onChange={(e) =>
                                 handleOnChange(e.target.value, "type")
                             }
                         >
-                            {types.map((type) => <MenuItem
-                                key={type}
-                                value={type}
-                            >
-                                {type}
-                            </MenuItem>)}
+                            {types.map((type) => (
+                                <MenuItem key={type} value={type}>
+                                    {type}
+                                </MenuItem>
+                            ))}
                         </Select>
                     </FormControl>
                 </Box>
             </Box>
-            <Divider variant="fullWidth" sx={{ mt: 3, mb: 3 }} />
-            <Box
-                sx={{
-                    marginTop: 3,
-                    paddingLeft: 6,
-                    width: 500,
-                    display: "flex",
-                    justifyContent: "center",
-                    flexDirection: "column"
-                }}
-            >
-                <Typography variant="subtitle1" sx={{ mb: 2 }}>Categoria</Typography>
-                <FormControl required sx={{ width: 400 }}>
-                    <InputLabel id="category">Categoria</InputLabel>
-                    <Select
-                        labelId="category"
-                        id="select-category"
-                        label="Categoria"
-                        value={valuesInputs?.category !== "" ? valuesInputs.category.name : ""}
-                        onChange={(e) =>
-                            handleOnChange(e.target.value, "category")
-                        }
-                    >
-                        {categorys.map((category) => (
-                            <MenuItem key={category.name} value={category.name}>
-                                {category.name}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-            </Box>
-        </FormDetailsTeam >
+        </FormDetailsTeam>
     );
 }
 
@@ -156,7 +112,7 @@ const FormDetailsTeam = styled("form")(() => ({
     marginTop: "10px",
     background: "#FFFFFF",
     width: "100%",
-    height: 400,
+    height: 300,
     boxSizing: "border-box",
     boxShadow: "0 3px 10px rgb(0 0 0 / 0.2)",
     borderRadius: "5px",
