88c33493316879d013460392eb8cbe6685caec82
lucasscabral <lucas.9cabral@gmail.com>
2024-04-30 19:40:35 -0300

    Refactor: Ajusta quantidade de apurações para 10 por dia.

--- frontend/src/app/views/screenskit/verification/formsTableCreate/FormsTable.jsx
+++ frontend/src/app/views/screenskit/verification/formsTableCreate/FormsTable.jsx
@@ -11,7 +11,7 @@ import Inputs from "./Inputs";
 import instance from "../../../../../axios"
 import Notiflix from "notiflix";
 
-const SIZE_INPUTS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+const SIZE_INPUTS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13,14,15,16,17,18,19,20]
 
 export default function FormsTable({ typeEditOrCreateForm, dataApurathion, setDataApurathion, modificationInputTeam }) {
     const [onSubmit, setOnSubmit] = useState(false);
